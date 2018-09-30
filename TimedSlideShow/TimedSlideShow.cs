090d6818653da4cf0df93c3d36684653b84cbb25
Mike Suttom <mike.j.sutton@tx.rr.com>
2018-09-30 14:43:56 -0500

    Rename TimedSlideShow.cs to TimedSlideShow/TimedSlideShow.cs

--- TimedSlideShow/TimedSlideShow.cs
+++ TimedSlideShow/TimedSlideShow.cs
@@ -1,11 +1,11 @@
 /* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
  * When using this content, you must:
- * •    Acknowledge that the content is from the Sansar Knowledge Base.
- * •    Include our copyright notice: "© 2017 Linden Research, Inc."
- * •    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
- * •    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * â€¢    Acknowledge that the content is from the Sansar Knowledge Base.
+ * â€¢    Include our copyright notice: "Â© 2017 Linden Research, Inc."
+ * â€¢    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * â€¢    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
  * For example:
- * "This work uses content from the Sansar Knowledge Base. © 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ * "This work uses content from the Sansar Knowledge Base. Â© 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
  */
 
 using Sansar.Script;
